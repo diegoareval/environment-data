dabe48d51dbed7df680f1a1dcf72b0a954c00a5c
diego2000 <diego2000@gmail.com>
2020-08-28 19:03:59 -0600

    executing bat file from python

--- MacAdress.py
+++ MacAdress.py
@@ -1,8 +1,21 @@
 from uuid import getnode as get_mac
+import subprocess
+import sys
+import os
 
+mac = get_mac()
 def format_mac_adress(mac):
   return ':'.join(("%012X" % mac)[i:i+2] for i in range(0, 12, 2))
 
-mac = get_mac()
+def get_path():
+  return '{0}\\test.bat'.format(os.path.dirname(os.path.abspath(__file__)))
 
-print(format_mac_adress(mac))
+
+def execute_bat():
+  fullpath = get_path()
+  test = subprocess.call([r'{0}'.format(fullpath)])
+  
+
+execute_bat()
+
+
